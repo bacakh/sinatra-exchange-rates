d241140eb9c3051a851efbbc74f6aabac496f62c
bacakh <bacakh0@chicagobooth.edu>
2025-02-07 03:00:47 +0000

    initial commit

--- app.rb
+++ app.rb
@@ -1,21 +1,29 @@
 require "sinatra"
 require "sinatra/reloader"
 require "http"
+require "dotenv/load"
 
 get("/") do
-  @raw_response = HTTP.get(https://api.exchangerate.host/list?access_key=EXCHANGE_RATE_KEY
-  )
+  @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}")
 
   @string_response = @raw_response.to_s
-  @parsed_response = JSON.parse@string_response
+  @parsed_response = JSON.parse(@string_response)
 
-  @currencies = @parsed_response.fetch("currencies")
+  @currencies = @parsed_response.fetch("currencies").keys
   erb(:homepage)
 end
 
 
-get("/:first_symbol") do
-@the_symbol = params.fetch("first_symbol")
+get("/:from_currency") do
+@the_symbol = params.fetch("from_currency")
 
 erb(:step_one)
 end
+
+
+get("/:from_currency/:to_currency") do
+  @from = params.fetch("from_currecy")
+  @to = params.fetch("to_currecy")
+
+  erb(:step_two)
+  end
