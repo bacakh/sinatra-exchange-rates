fa75c6f93eb0396a32ed11c35d7a0e665adba7b8
bacakh <bacakh0@chicagobooth.edu>
2025-02-07 03:02:02 +0000

    cimmit 2

--- app.rb
+++ app.rb
@@ -16,6 +16,12 @@ end
 
 get("/:from_currency") do
 @the_symbol = params.fetch("from_currency")
+@raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}")
+
+@string_response = @raw_response.to_s
+@parsed_response = JSON.parse(@string_response)
+
+@currencies = @parsed_response.fetch("currencies").keys
 
 erb(:step_one)
 end
