fb9af3a3a89d52efc774c97c5c3cd40f9ff4cb2f
bacakh <bacakh0@chicagobooth.edu>
2025-02-08 17:24:36 +0000

    p58 commit

--- app.rb
+++ app.rb
@@ -28,16 +28,17 @@ end
 
 
 get("/:from_currency/:to_currency") do
-  @from = params.fetch("from_currecy")
-  @to = params.fetch("to_currecy")
+  @from = params.fetch("from_currency")
+  @to = params.fetch("to_currency")
 
-  @the_symbol = params.fetch("from_currency")
-@raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}")
+  @url = "https://api.exchangerate.host/convert?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY").chomp}&from=#{@from}&to=#{@to}&amount=1"
 
-@string_response = @raw_response.to_s
-@parsed_response = JSON.parse(@string_response)
+  @raw_response = HTTP.get(@url)
 
-@currencies = @parsed_response.fetch("currencies").keys
+  @string_response = @raw_response.to_s
+  @parsed_response = JSON.parse(@string_response)
+
+  @amount = @parsed_response.fetch("result")
 
   erb(:step_two)
   end
